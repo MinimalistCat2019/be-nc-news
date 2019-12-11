7b8e6d524140d02e12f73ca6c8a78f727916f2f5
MinimalistCat2019 <48767239+MinimalistCat2019@users.noreply.github.com>
2019-12-11 11:54:02 +0000

    updated cors and fixed some double quoting slips in endpoints.js

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const {
   handleServerErrors,
   handleMethodNotFound
 } = require("./error-handling/index");
-
 const app = express();
-
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
 
 app.get('/api')
